a0704c3cafbf43274e84b7934f78c224eeec52ae
Omid07 <omidchowdhury@gmail.com>
2016-05-31 16:09:11 +0600

    main page UI

--- app/src/main/java/com/example/omid/rss_5/MainActivity.java
+++ app/src/main/java/com/example/omid/rss_5/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.omid.rss_5;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.category_recycler_view);
+        Resources resources = getResources();
+        String [] categories = resources.getStringArray(R.array.categories);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new CategoryAdapter(categories);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
